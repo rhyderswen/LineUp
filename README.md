ecc5e1a0a01e84109d862faeb6d436a09a329d31
x <178953228+lndrysrvcs@users.noreply.github.com>
2026-01-26 18:42:51 -0500

    Add initial project setup with backend, frontend, and API glue for interconnect.

---
+++ README.md
@@ -0,0 +1,5 @@
+# LineUp
+LineUp is a scheduling tool for when you need 100% coverage over a period of time.
+
+### API client autogeneration
+Run `GEN=true dotnet build` in LineUp.Backend to generate the API client in the lineup-client.
---
+++ lineup-client/openapi-ts.config.ts
@@ -0,0 +1,27 @@
+import { defineConfig } from '@hey-api/openapi-ts';
+
+export default defineConfig({
+    client: '@hey-api/client-axios',
+    input: '../openapi-spec/api-spec.json',
+    output: {
+        postProcess: ['eslint', 'prettier'],
+        path: './src/api-client',
+    },
+    plugins: [
+        '@hey-api/schemas',
+        {
+            dates: true,
+            name: '@hey-api/transformers',
+        },
+        {
+            enums: 'javascript',
+            name: '@hey-api/typescript',
+        },
+        {
+            name: '@hey-api/sdk',
+            transformer: true,
+        },
+        'zod',
+        '@tanstack/react-query',
+    ],
+});
