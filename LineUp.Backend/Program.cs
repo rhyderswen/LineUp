87a3458ce39e896f3ea35aafccfdd5cb69fa054d
Eddie Rodriguez <178953228+lndrysrvcs@users.noreply.github.com>
2026-01-30 18:41:02 -0500

    Apply code formatting standards with Prettier and Sharpier (#2)
    * Add StyleCop.Analyzers and update code formatting
    
    * Set up prettier and commit hooks for prettier
    
    * apply prettier code style + disable eslint rules that conflict with prettier
    
    * fix husky hooks
    
    * fix husky hooks
    
    * migrate from stylecop to Sharpier
    
    * migrate from stylecop to Sharpier

--- LineUp.Backend/Program.cs
+++ LineUp.Backend/Program.cs
@@ -16,26 +16,38 @@ app.UseHttpsRedirection();
 
 var summaries = new[]
 {
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    "Freezing",
+    "Bracing",
+    "Chilly",
+    "Cool",
+    "Mild",
+    "Warm",
+    "Balmy",
+    "Hot",
+    "Sweltering",
+    "Scorching",
 };
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+app.MapGet(
+        "/weatherforecast",
+        () =>
+        {
+            var forecast = Enumerable
+                .Range(1, 5)
+                .Select(index => new WeatherForecast(
+                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                    Random.Shared.Next(-20, 55),
+                    summaries[Random.Shared.Next(summaries.Length)]
+                ))
+                .ToArray();
+            return forecast;
+        }
+    )
+    .WithName("GetWeatherForecast");
 
 app.Run();
 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+internal record WeatherForecast(DateOnly date, int temperatureC, string? summary)
 {
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+    public int TemperatureF => 32 + (int)(this.temperatureC / 0.5556);
 }
--- lineup-client/src/main.tsx
+++ lineup-client/src/main.tsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { Auth0Provider } from "@auth0/auth0-react";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <Auth0Provider
+      domain="dev-hwakn360ihxv0db6.us.auth0.com"
+      clientId="A1gTh2lbIMwTZHv6mO4B9OK0sAvfqtnC"
+      authorizationParams={{ redirect_uri: window.location.origin }}
+    >
+      <App />
+    </Auth0Provider>
   </StrictMode>,
-)
+);
