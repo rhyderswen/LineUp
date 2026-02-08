795d0241ae982ea9797f8be4b0780b31010fe940
JMarkowski17 <cle2joey@gmail.com>
2026-02-07 19:00:12 -0500

    Logged in Home Screen Created + Color changes

---
+++ lineup-client/src/components/Table.tsx
@@ -0,0 +1,37 @@
+interface TableProps<T> {
+  headers: string[];
+  data: T[];
+  renderRow: (item: T) => React.ReactNode;
+  columnWidths?: string[];
+}
+
+const Table = <T,>({
+  headers,
+  data,
+  renderRow,
+  columnWidths,
+}: TableProps<T>) => {
+  return (
+    <table className="scheduleTable">
+      <colgroup>
+        {columnWidths?.map((width, index) => (
+          <col key={index} style={{ width }} />
+        ))}
+      </colgroup>
+      <thead>
+        <tr>
+          {headers.map((header) => (
+            <th key={header}>{header}</th>
+          ))}
+        </tr>
+      </thead>
+      <tbody>
+        {data.map((item, index) => (
+          <tr key={index}>{renderRow(item)}</tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+export default Table;
--- lineup-client/src/pages/LoggedInHome.tsx
+++ lineup-client/src/pages/LoggedInHome.tsx
@@ -1,11 +1,72 @@
 import { useAuth0 } from "@auth0/auth0-react";
+import Table from "../components/Table";
+import CopyableLink from "../components/CopyableLink";
+
+interface TableData {
+  name: string;
+  respondents: number;
+  link: string;
+}
+
+// Example data for now, will need to reference user data for actual entries
+const items: TableData[] = [
+  {
+    name: "Club Tabling",
+    respondents: 18,
+    link: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+  },
+  {
+    name: "Radio Shows",
+    respondents: 24,
+    link: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+  },
+];
+
+const headers = ["Name", "Respondents", "Link", "Schedule"];
+
+const renderRow = (row: TableData) => (
+  <>
+    <td>{row.name}</td>
+    <td>{row.respondents}</td>
+    <td>
+      <CopyableLink url={row.link} />
+    </td>
+    <td className="btnCol">
+      <button
+        className="scheduleBtn"
+        onClick={() => {
+          // TODO: make this actually go to the appropriate schedule
+        }}
+      >
+        View/Edit
+      </button>
+    </td>
+  </>
+);
 
 const LoggedInHome = () => {
   const { user } = useAuth0();
   console.log(user);
   return (
     <div className="home">
-      Congrats, <b>{user?.name}</b>! You're logged in!
+      <b>{user?.name}</b>'s Home
+      <div className="tableHeader">
+        <b className="tableTitle">My Schedules </b>
+        <button
+          className="rightButton"
+          onClick={() => {
+            //TODO: make this button go to the NewSchedule screen
+          }}
+        >
+          New Schedule
+        </button>{" "}
+      </div>
+      <Table
+        headers={headers}
+        data={items}
+        renderRow={renderRow}
+        columnWidths={["25%", "25%", "30%", "20%"]}
+      />
     </div>
   );
 };
