63632214d7a9452fe1cff2628d9846caf729cd05
x <178953228+lndrysrvcs@users.noreply.github.com>
2026-03-07 22:01:58 -0500

    add csharpier across repo and also add packages for better unit tests

--- LineUp.EndToEndTests/IntegrationTest1.cs
+++ LineUp.EndToEndTests/IntegrationTest1.cs
@@ -5,13 +5,16 @@ namespace LineUp.EndToEndTests;
 public class IntegrationTest1
 {
     private static readonly TimeSpan DefaultTimeout = TimeSpan.FromSeconds(30);
-    
+
     [Fact]
     public async Task GetWebResourceRootReturnsOkStatusCode()
     {
         // Arrange
         var cancellationToken = CancellationToken.None;
-        var appHost = await DistributedApplicationTestingBuilder.CreateAsync<Projects.LineUp_AppHost>(cancellationToken);
+        var appHost =
+            await DistributedApplicationTestingBuilder.CreateAsync<Projects.LineUp_AppHost>(
+                cancellationToken
+            );
         appHost.Services.AddLogging(logging =>
         {
             logging.SetMinimumLevel(LogLevel.Debug);
@@ -24,15 +27,19 @@ public class IntegrationTest1
         {
             clientBuilder.AddStandardResilienceHandler();
         });
-    
-        await using var app = await appHost.BuildAsync(cancellationToken).WaitAsync(DefaultTimeout, cancellationToken);
+
+        await using var app = await appHost
+            .BuildAsync(cancellationToken)
+            .WaitAsync(DefaultTimeout, cancellationToken);
         await app.StartAsync(cancellationToken).WaitAsync(DefaultTimeout, cancellationToken);
-    
+
         // Act
         using var httpClient = app.CreateHttpClient("web");
-        await app.ResourceNotifications.WaitForResourceHealthyAsync("web", cancellationToken).WaitAsync(DefaultTimeout, cancellationToken);
+        await app
+            .ResourceNotifications.WaitForResourceHealthyAsync("web", cancellationToken)
+            .WaitAsync(DefaultTimeout, cancellationToken);
         using var response = await httpClient.GetAsync("/", cancellationToken);
-    
+
         // Assert
         Assert.Equal(HttpStatusCode.OK, response.StatusCode);
     }
