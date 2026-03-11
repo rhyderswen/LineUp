699c9b16e5b138f0fef17c75e4a57cbce8ed3370
x <178953228+lndrysrvcs@users.noreply.github.com>
2026-03-10 23:49:14 -0400

    fix mistakenly serialized schedule attribute

--- LineUp.Backend/Models/Forms/Form.cs
+++ LineUp.Backend/Models/Forms/Form.cs
@@ -1,8 +1,12 @@
+using LineUp.Backend.Attributes;
+
 namespace LineUp.Backend.Models.Forms;
 
 public class Form
 {
     public int Id { get; set; }
     public ICollection<FormQuestion> Questions { get; set; } = new List<FormQuestion>();
+
+    [JsonDoNotSerialize]
     public required Schedule Schedule { get; set; }
 }
