bdb2816fa55d8e86e7aa0c257b61414df7706562
Rembi Time <rembitime@gmail.com>
2026-03-16 20:34:48 -0400

    Fixed Frontend Aspire Deployment

--- LineUp.AppHost/AppHost.cs
+++ LineUp.AppHost/AppHost.cs
@@ -7,19 +7,33 @@ builder.AddDockerComposeEnvironment("env");
 
 var postgres = builder.AddPostgres("postgres")
     .WithDataVolume();
+
 var postgresdb = postgres.AddDatabase("postgresdb");
 
 var api = builder.AddProject<LineUp_Backend>("api")
     .WaitFor(postgresdb)
     .WithReference(postgresdb);
 
-var web = builder.AddViteApp("web", "../lineup-client")
-    .WithEnvironment("PORT", "5173")
-    .WithExternalHttpEndpoints()
-    .WithEndpoint("http", endpointAnnotation => endpointAnnotation.Port = 5173)
-    .WithPnpm()
-    .WithReference(api)
-    .WaitFor(api)
-    .AsDeployableService();
+IResourceBuilder<IResourceWithEndpoints> web;
+
+if (builder.ExecutionContext.IsRunMode)
+{
+    web = builder.AddViteApp("web", "../lineup-client")
+        .WithPnpm()
+        .WithHttpEndpoint(port: 5173, env: "PORT")
+        .WithExternalHttpEndpoints()
+        .WithReference(api)
+        .WaitFor(api)
+        .AsDeployableService();
+}
+else
+{
+    web = builder.AddDockerfile("web", "../lineup-client")
+        .WithHttpEndpoint(port: 8080, targetPort: 80, env: "PORT")
+        .WithExternalHttpEndpoints()
+        .WithEnvironment("VITE_API_URL", api.GetEndpoint("http"))
+        .WithReference(api)
+        .WaitFor(api);
+}
 
 builder.Build().Run();
