969a86083ce23d60628aa36777e64a6af40ea9c5
Eddie Rodriguez <178953228+lndrysrvcs@users.noreply.github.com>
2026-03-18 01:38:54 -0400

    fix seeding by navigation property migration challenge: possible

---
+++ LineUp.Backend/Migrations/20260318053812_FixSeedingByNavigationProperty.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LineUp.Backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixSeedingByNavigationProperty : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ShiftAssignments_Schedules_ScheduleId",
+                table: "ShiftAssignments");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ScheduleId",
+                table: "ShiftAssignments",
+                type: "integer",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "integer");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ShiftAssignments_Schedules_ScheduleId",
+                table: "ShiftAssignments",
+                column: "ScheduleId",
+                principalTable: "Schedules",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ShiftAssignments_Schedules_ScheduleId",
+                table: "ShiftAssignments");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ScheduleId",
+                table: "ShiftAssignments",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ShiftAssignments_Schedules_ScheduleId",
+                table: "ShiftAssignments",
+                column: "ScheduleId",
+                principalTable: "Schedules",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
